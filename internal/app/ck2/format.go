0384e3d1d1f7bdc5669711dfc1a12a6a5dfd2611
unlomtrois <vadim.kuz02@gmail.com>
2022-09-01 01:25:06 +0300

    move parsing types to separate file

--- internal/app/ck2/format.go
+++ internal/app/ck2/format.go
@@ -17,35 +17,6 @@ import (
 // 	file *os.File `json:"file"`
 // }
 
-type CK2Parser struct {
-	Entities  []*Entity `json:"entities"`
-	Scope     *Entity   `json:"scope"`
-	PrevScope *Entity
-	Depth     int `json:"depth"`
-}
-
-type Entity struct {
-	Name     string           `json:"name"`
-	Elements []*EntityElement `json:"elements"`
-}
-
-type EntityType string
-
-const (
-	Property EntityType = "Property"
-	Block               = "Block"
-)
-
-type EntityElement struct {
-	// element is either a Block or a Property
-	Type EntityType `json:"type"`
-	// not nil if type is Block
-	Data []*EntityElement `json:"data"`
-	// not nil if type is Property
-	Key   string `json:"key"`
-	Value string `json:"value"`
-}
-
 func Parse(f *os.File) error {
 	buf := &bytes.Buffer{}
 	tee := io.TeeReader(f, buf)
