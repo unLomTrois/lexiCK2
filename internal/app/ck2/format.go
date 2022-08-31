a31f5916a6fdb8bf473888ad5a4763d5059c47d5
unlomtrois <vadim.kuz02@gmail.com>
2022-09-01 02:32:28 +0300

    replace ioutil to os

--- internal/app/ck2/format.go
+++ internal/app/ck2/format.go
@@ -6,7 +6,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"os"
 )
 
@@ -68,7 +67,7 @@ func Parse(f *os.File) error {
 
 	aJSON, _ := json.MarshalIndent(parser, "", "  ")
 	fmt.Printf("JSON Print - \n%s\n", string(aJSON))
-	_ = ioutil.WriteFile("tmp/simple.json", aJSON, 0644)
+	_ = os.WriteFile("tmp/simple.json", aJSON, 0644)
 	w.Flush()
 
 	return nil
